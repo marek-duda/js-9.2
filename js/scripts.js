7d416ab864fee4f41b5b524f50ec3e119fcd5f2c
Maro <grafika.marekduda@gmail.com>
2017-06-04 22:56:02 +0200

    added semicolons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 var femalNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femalNames.concat(maleNames);
-var newName = 'Marian'
+var newName = 'Marian';
 
 if (allNames.indexOf(newName) === -1) {
   allNames[8] = 'Marian';
 } 
 
-console.log(allNames)
+console.log(allNames);
